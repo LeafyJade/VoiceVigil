1cf9827bb624122fd39a5e6b64bd7fe2f0f410b5
Jade Gregoire <jade.gregoire2021@gmail.com>
2023-11-17 14:27:15 -0500

    fixed issue with bot not recognizing users in userlist, added voice channel kicking and banning to the heirarchy, added user warning messages, and added banned words

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const myIntents = [
 ];
 const client = new Client({ intents: myIntents });
 addSpeechEvent(client);
-// User List to store the times that said baned word
+// User List to store the times they said a banned word
 global.userCount = {}
 
 // Banned words list
@@ -26,6 +26,13 @@ const bannedWords = new Set();
 bannedWords.add("bubbles");
 bannedWords.add("s***");
 bannedWords.add("f***");
+bannedWords.add("f*"); // fucker
+bannedWords.add("ass");
+bannedWords.add("a**"); // asshole
+bannedWords.add("motherfuker");
+bannedWords.add("b**"); // bitch
+bannedWords.add("bastard");
+bannedWords.add("damn");
 
 // SPEECH TRANSCRIPTION 
 client.on(SpeechEvents.speech, (msg) => {
@@ -34,42 +41,57 @@ client.on(SpeechEvents.speech, (msg) => {
 		return;
 	}
 	
-	const channel = client.channels.cache.find(channel => channel.id === "1169407845838630923"); // hardcoded transcription text channel ID
+	const channel = client.channels.cache.find(channel => channel.id === "1169407845838630923"); // hardcoded transcription text channel ID.
+	const warning_channel = client.channels.cache.find(channel => channel.id === "1175147876456861756"); // harded coded warning channel for users to see their mess-ups. 
 	channel.send(msg.author.username + ": " + msg.content);
 	// msg.author.send(msg.content);
-  
-  // Convert message to array by splitting
-  const messageArray = msg.content.split(" ");
-  for (let i = 0; i < messageArray.length; i++) {
-    // Convert each word to lowercase for comparison
-    messageArray[i] = messageArray[i].toLowerCase();
-    if (bannedWords.has(messageArray[i])) {
-      // Detect if word is in banned list
-	  const member = msg.guild.members.cache.get(msg.author.id);
-      if(userCount[msg.author.username]==0){
-		member.voice.setMute(true).then(()=>{
-			channel.send(msg.author.username + " said banned word " + messageArray[i] +" firstly, has been muted");
-			userCount[msg.author.username]= userCount[msg.author.username]+1;
-		})
-	  }
-	  else if(userCount[msg.author.username]==1){
-		member.kick().then(()=>{
-			channel.send(msg.author.username + " said banned word " + messageArray[i] +" secondly, has been kicked");
-			userCount[msg.author.username]= userCount[msg.author.username]+1;
-	  })
+
+	// Convert message to array by splitting
+	const messageArray = msg.content.split(" ");
+	for (let i = 0; i < messageArray.length; i++) {
+		// Convert each word to lowercase for comparison
+		messageArray[i] = messageArray[i].toLowerCase();
+
+		if (bannedWords.has(messageArray[i])) { // Detect if word is in banned list
+			
+			const member = msg.guild.members.cache.get(msg.author.id);
+			if (!(msg.author.username in userCount)) {
+				userCount[msg.author.username] = 0;
+			}
+
+			if (userCount[msg.author.username] == 0){
+				warning_channel.send(msg.author.toString() + ", you said " + messageArray[i] + ", which is flagged as a bad word. You have been warned, and will be muted if you repeat this offence. You may appeal this with a moderator.");
+				channel.send(msg.author.username + " said this banned word: " + messageArray[i] + ". This is a first warning.");
+				userCount[msg.author.username] = userCount[msg.author.username] + 1;
+			} else if (userCount[msg.author.username] == 1){
+				member.voice.setMute(true).then(()=>{
+					warning_channel.send(msg.author.toString() + ", you said " + messageArray[i] + ", which is flagged as a bad word. You have been muted, and will be kicked from the voice channel if you repeat this offence. You require a moderator to unmute you.");
+					channel.send(msg.author.username + " said this banned word: " + messageArray[i] + ". This is a second warning. The user has been muted.");
+					userCount[msg.author.username] = userCount[msg.author.username] + 1;
+				})
+			} else if (userCount[msg.author.username] == 2){
+				member.voice.setMute(true);
+				member.voice.disconnect().then(()=>{
+					warning_channel.send(msg.author.toString() + ", you said " + messageArray[i] + ", which is flagged as a bad word. You have been kicked from the voice channel and muted, and will be kicked from the server if you repeat this offence. You require a moderator to unmute you.");
+					channel.send(msg.author.username + " said this banned word: " + messageArray[i] + ". This is a third warning. The user has been kicked from the voice channel and muted.");
+					userCount[msg.author.username] = userCount[msg.author.username] + 1;
+				})
+			} else if (userCount[msg.author.username] == 3){
+				member.kick().then(()=>{
+					msg.author.send(msg.author.username + ", you said " + messageArray[i] + ", which is flagged as a bad word. Since this was your fourth warning, you have been kicked from the server. You may re-join the server, but will be banned if you have another offence. You may appeal this with a moderator to reset your warnings.");
+					channel.send(msg.author.username + " said this banned word: " + messageArray[i] + ". This is a fourth warning. The user has been kicked from the server."); 
+					userCount[msg.author.username] = userCount[msg.author.username] + 1;
+				})
+			} else {
+				member.ban().then(()=>{
+					msg.author.send(msg.author.username + ", you said " + messageArray[i] + ", which is flagged as a bad word. Since this was your fifth warning, you have been banned from the server. You may attempt to appeal this with a moderator if you wish to re-join again.");
+					channel.send(msg.author.username + " said this banned word " + messageArray[i] + ". This has been the user's fifth offense. The user has been banned from the server.");
+				})
+			}
+			break;
+		}
 	}
-	  else {
-		member.kick().then(()=>{
-			channel.send(msg.author.username + " said banned word " + messageArray[i] +" thirdly, has been banned");
-	  })
-	  //channel.send(msg.author.username + " said banned word " + messageArray[i] +"!");
-      //console.log(msg.author.username + " said banned word " + messageArray[i] +"!");
-    }
-	break;
-  }
-}
-}
-);
+});
 
 // add Event handlers
 const eventsPath = path.join(__dirname, 'events');
